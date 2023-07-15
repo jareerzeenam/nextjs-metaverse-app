2f23e5729ae125bed94fc47ca72ccc35ae6d3694
Jareer Zeenam <jareerzeenam.28@gmail.com>
2023-07-16 00:28:18 +0530

    Initial project structure done

---
+++ app/head.js
@@ -0,0 +1,12 @@
+const Head = () => (
+  <>
+    <title>Jareer's Metaversus App</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1"
+    />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+);
+
+export default Head;
